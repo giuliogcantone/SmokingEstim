326970d8b976d128f9bbf3e57b968a2089224dd3
giuliogcantone <prgcan@gmail.com>
2022-02-28 01:23:05 -0500

    ok

--- AlexHayes.R
+++ AlexHayes.R
@@ -20,37 +20,37 @@ do.call(
 ### B
 
 map_dfc(
-  seq(.1,.9,.1),
+  seq(.05,.95,.1),
   function(.x) {
-    (dbeta(seq(.1,.9,.1),.x*10,10-.x*10))/
-      sum(dbeta(seq(.1,.9,.1),
+    (dbeta(seq(.05,.95,.1),.x*10,10-.x*10))/
+      sum(dbeta(seq(.05,.95,.1),
                 .x*10,
                 10-.x*10))}
 ) %>% as.matrix() -> B
 
-###repeat 3 times
-(B+mean(B))/sum((B+mean(B))) -> B
+map_dfc(
+  seq(.05,.95,.1),
+  function(.x) {
+    (pbeta(seq(.05,.95,.1),.x*10,10-.x*10))}
+) %>% as.matrix() -> B
 
-###
 forceSymmetric(B) %>% as.matrix() -> B
+B/sum(B) -> B
+superheat::superheat(B)
 
-# checks
-
-superheat:superheat(B)
 View(B)
-
-B[1,] %>% sum()
-B[3,] %>% sum()
-B[5,] %>% sum()
-
+  
 ### latent sbm
 library(VGAM)
 
+dbetabinom(0:9,9,.22,.18) %>% barplot()
+(rbetabinom(10000,9,.22,.18)+.5) %>% mean()
+
 latent <- fastRG::sbm(
   n = V(graph1) %>% length(),
-  pi = dbetabinom(0:8,8,.2,.2),
+  pi = dbetabinom(0:9,9,.22,.18),
   B = B,
-  expected_degree = 20,
+  expected_degree = 500,
   sort_nodes = T
 )
 
@@ -62,6 +62,9 @@ graph2 %>%
   mutate(
     block = latent$z
   ) %>% as_tibble() -> nodes
+
+graph2 %>%activate(edges) %>%
+  sample_n(length(E(graph2)) / 25) -> graph2
 
 ### Checks
 
@@ -100,10 +103,9 @@ graph2 %>%
                rename(to_block = block) %>%
                group_by(from_block, to_block) %>% group_by(to_block) %>%
                count()) %>% ungroup %>% transmute(block = from_block,
-                                                  n_links = (to+n)/2) %>%
+                                                  n_links = (to+n)) %>%
   inner_join(nodes %>%
                group_by(block) %>% count(), by = "block")
-
 
 graph2 %>%
   activate(edges) %>%
@@ -125,6 +127,62 @@ graph1 %>% as_tbl_graph() %>%
   ) %>%
   mutate(name = as.character(name)) %>%
   inner_join(graph2 %>% activate(nodes) %>%
-               as_tibble(), by = "name")
+               as_tibble(), by = "name") %>%
+  bind_edges(graph2%>%activate(edges)%>%as_tibble) %>%
+  activate(nodes) %>%
+  select(-nucleus_size) %>%
+  mutate(
+    nucleus = sample(nucleus, replace = F),
+    k = centrality_degree(),
+    neighs = local_members()) %>%
+  group_by(nucleus) %>%
+  mutate(nuclear_prop = (rbetabinom(1,9,.2,.2)+.5)/10) %>%
+  group_by(block) %>%
+  mutate(block_prop = str_remove(block,"block") %>%
+           as.integer(),
+         block_prop = abs(block_prop - 10.5)/10) %>%
+  ungroup() %>% group_by(name) %>%
+  mutate(
+    p = mean(c(nuclear_prop,block_prop)),
+    smoker = rbinom(1,1,p)) -> pop
 
-graph2 
+pop %>% as_tibble() %>% View()
+
+### DIAGNOSTIC: Homophily in the pop
+
+pop %>% activate(nodes) %>% mutate(name = as.character(name)) %>%
+  as_tibble() -> pop_nodes
+
+pop %>% activate(edges) %>%
+  as_tibble() %>%
+  mutate(from = as.character(from),
+         to = as.character(to)) -> pop_edges
+
+pop_nodes %>% group_by(smoker) %>% summarize(k = mean(k))
+
+pop_edges %>% rename(name = from) %>%
+  inner_join(pop_nodes %>% select(name,smoker), by = "name") %>%
+  rename(from = name,
+         smoker_from = smoker,
+         name = to) %>%
+  inner_join(pop_nodes %>% select(name,smoker), by = "name") %>%
+  rename(to = name,
+         smoker_to = smoker) %>%
+  mutate(smoke_sum = smoker_to + smoker_from) %>%
+  group_by(smoke_sum) %>% count() %>%
+  ungroup %>% mutate(f = n/sum(n))
+
+# Diagnostic: Degree connectivity
+
+pop %>% activate(nodes) %>% as_tibble() %>%
+  mutate(p = as.character(p)) %>% group_by(p) %>% summarise(
+    n(),
+    mean(k)
+    ) %>% View()
+
+### pop
+
+pop %>% activate(nodes) %>% ungroup() %>%
+  mutate(smok.neighs = sum(
+           (name %in% neighs))) %>% as_tibble() %>%
+  View()
