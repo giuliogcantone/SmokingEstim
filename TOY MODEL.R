acbf08c3bba7a662d736328cb1ee143f999da976
giuliogcantone <prgcan@gmail.com>
2022-03-13 22:13:39 -0400

    ok

--- TOY MODEL.R
+++ TOY MODEL.R
@@ -4,22 +4,19 @@ library(tidygraph)
 library(VGAM)
 
 #g1
-n = 46
-g1 = list()
+n = 23
 
 tibble(k = rpois(n,1.2)+1,
-       nucleus = 1:n) -> g1$n
+       nucleus = 1:n) -> g1
 
 tibble(
   w = .4,
-  nucleus = rep(g1$n$nucleus,g1$n$k)) %>%
-  mutate(name = cur_group_rows() %>% sample()) %>%
+  nucleus = rep(g1$n$nucleus,g1$n$k)  %>% sample()) %>%
+  mutate(name = cur_group_rows()) %>%
   group_by(nucleus) %>%
   mutate(alpha = rbetabinom(1,9,.225,.3),
          alpha = (alpha/10) + .05) %>%
-  ungroup() -> g1$n
-
-g1$n %>%
+  ungroup() %>%
   with(
     .,
     do.call(
@@ -36,10 +33,9 @@ g1$n %>%
         split(name, nucleus)
       )
     )
-  ) %>% as_tibble() %>% mutate(color = "red") -> g1$e
-
-tbl_graph(g1$n,g1$e,directed = F) -> g1
-
+  ) %>% as_tibble() %>% mutate(color = "red") %>%
+  tbl_graph(g1,.,directed = F) %>%
+  activate(nodes) %>% arrange(name) -> g1
 
 # g2
 library(Matrix)
@@ -47,23 +43,13 @@ library(Matrix)
 map_dfc(
   seq(1,10,1),
   function(.x) {
-    (dpois(seq(1,10,1),.x))}
+    (dnorm(seq(1,10,1),.x,.x))}
 ) |> as.matrix() |>
-  forceSymmetric() |>
+  forceSymmetric(uplo = "U") |>
   as.matrix() -> B
 
 B/sum(B) -> B
-colnames(B) <- (seq(.05,.95,.1))
-rownames(B) <- (seq(.05,.95,.1))
-
-(B^.4 / sum(B^.4)) %>%
-  superheat::superheat(.,
-                       X.text = round(., 3),
-                       X.text.size = 5,
-                       legend = FALSE,
-                       X.text.col = "black")
-
-(B^.4 / sum(B^.4)) -> G
+(B^.5 / sum(B^.5)) -> G
 
 #
 library(fastRG)
@@ -72,27 +58,23 @@ latent <- fastRG::sbm(
   n = V(g1) %>% length(),
   pi = dbetabinom(0:9,9,.225,.3),
   B = G,
-  expected_degree = 7,
+  expected_degree = 13,
   sort_nodes = T
 )
 
-latent$z %>% length()
-
-g2 <- sample_tidygraph(latent) %>%
-  activate(nodes) %>%
-  mutate(
-    beta = latent$z
-  ) %>% mutate(
-    name = as.integer(name),
-    beta = abs((str_remove(beta,"block") %>%
-                  as.integer()) - 10)/10 + .05
-  ) -> g2
-
+sample_tidygraph(latent) %>%
+  activate(nodes) %>% mutate(name = as.integer(name)) -> g2
+                             
 graph_join(g1,g2) %>% as_tbl_graph() %>%
   activate(edges) %>% mutate(
     color = ifelse(is.na(color),"blue","red")
   ) %>%
-  activate(nodes) %>% group_by(name) %>%
+  activate(nodes) %>%
+  mutate(
+    beta = latent$z,
+    beta = abs((str_remove(beta,"block") %>%
+                  as.integer()) - 10)/10 + .05
+  ) %>% group_by(name) %>%
   mutate(
     e = (alpha*w)+beta*(1-w),
     smoker = rbinom(1,1,e),
@@ -107,44 +89,32 @@ g %>% activate(nodes) %>% as_tibble() %>%
 
 
 library(ggraph)
-g %>% ggraph(layout = "kk") +
-  geom_edge_hive(aes(colour = color,
+g %>% ggraph(layout = "stress") +
+  geom_edge_link(aes(colour = color,
                     width = color)) +
   geom_node_point(aes(fill = color),
                   size = 7, shape = 21, stroke = 1, color = 'black') +
-  scale_fill_manual(values = c("pink" = "pink","green" = "green")) +
-  scale_edge_color_manual(values = c("red" = "red","blue" = "blue"))+
+  scale_fill_manual(values = c("pink" = "white","green" = "forestgreen")) +
+  scale_edge_color_manual(values = c("red" = "red","blue" = "orange"))+
   geom_node_text(aes(label = name))+
   scale_edge_width_manual(values = c(
     "red" = 1,
     "blue" = .5)) +
-  theme_void()
+  theme_void() +
+  theme(legend.position="none") 
 
-?scale_edge_size
+ggsave("network.eps", device = "eps")
+  
+ggsave()
 
-g %>% activate(nodes) %>% as_tibble() %>% filter(nucleus == nucleus[name == 57])
+g %>% as.igraph()
+
+g %>% activate(nodes) %>% as_tibble() %>% filter(nucleus == nucleus[name == 8])
 g %>% activate(edges) %>% as_tibble() %>% filter(to == 57 | from == 57)
 
+### Diagnostica
 
-g1 %>% activate(nodes) %>% as_tibble() %>% filter(nucleus == nucleus[name == 78])
-g1 %>% activate(edges) %>% as_tibble() %>% filter(to == 78 | from == 78)
-
-
-g1 %>% ggraph(layout = "stress") +
-  geom_edge_link(aes(colour = color,
-                     width = color)) +
-  geom_node_point(size = 7, shape = 21, stroke = 1, color = 'black') +
-  scale_edge_color_manual(values = c("red" = "red","blue" = "blue"))+
-  geom_node_text(aes(label = name))+
-  scale_edge_width_manual(values = c(
-    "red" = 1,
-    "blue" = .5)) +
-  theme_void()
-
-g1 %>% as.igraph()
-g1 %>% activate(edges) %>% as_tibble() %>% filter(to == 18 | from == 18)
-
-g1 %>% activate(nodes) %>% arrange(name) %>% as.igraph
-g1 %>% activate(nodes)  %>% as.igraph
-
-g1 %>% activate(nodes)
+g %>% activate(nodes) %>%
+  mutate(k = centrality_degree()) %>%
+  as_tibble() %>% group_by(smoker) %>%
+  summarize('E(k)' = mean(k))
--- graphics.R
+++ graphics.R
@@ -10,182 +10,43 @@ ggplot(tibble(
   aes(x=z,y=y)) +
   geom_histogram(stat="identity") +
   theme_classic(base_size = 20) +
-  ylab("PMF")+
+  ylab("Probability")+
   xlab("Propensity score")
   geom_vline(aes(xintercept = 2.75), color = "red")
   annotate("text",x=4,y=.32,label="Expected Value",
            color = "red", size = 10)
-
+ggsave("PMF.eps")
+  
 (rbetabinom(10000,9,.225,.3)+.05) %>% mean()
 (rbetabinom(10000,9,.225,.3)+.05) %>% var()
 
-### MIXING
+### MIXING MATRIX
 library(Matrix)
 
 map_dfc(
   seq(1,10,1),
   function(.x) {
-    (dpois(seq(1,10,1),.x))}
-  ) |> as.matrix() |>
-  forceSymmetric() |>
-  as.matrix() -> B
-
-B/sum(B) -> B
-
-colnames(B) <- (seq(.05,.95,.1))
-rownames(B) <- (seq(.05,.95,.1))
-
-superheat::superheat(B,
-                     X.text = round(B, 3),
-                     X.text.size = 5,
-                     legend = FALSE,
-                     X.text.col = "black")
-
-
-(B^.2 / sum(B^.2)) %>%
-  superheat::superheat(.,
-                       X.text = round(., 3),
-                       X.text.size = 5,
-                       legend = FALSE,
-                       X.text.col = "black")
-
-
-### TOY NETWORK
-library(igraph)
-library(tidyverse)
-
-# Graph1
-do.call(
-  graph.disjoint.union,
-  lapply(
-    rpois(10,1.2)+1,
-    function(k) {
-      make_full_graph(k) %>% set_vertex_attr(
-        name = "nucleus_size", value = k)
-    }
-  )
-) %>%
-  set_vertex_attr(name = "nucleus", value = membership(components(.))) %>% 
-  set_vertex_attr(name = "name", value = seq(vcount(.))) %>%
-  set_edge_attr(name = "color", value = "red") %>% as_tbl_graph() %>%
-  activate(nodes) %>% mutate(name_ = sample(name)) -> graph1
-
-tbl_graph(graph1 %>% activate(nodes)
-          %>% as_tibble() %>% mutate(name = name_) %>%
-            select(-name_),graph1 %>%
-            activate(edges) %>%
-            as_tibble() %>%
-            mutate(name = from) %>%
-            inner_join(graph1 %>% activate(nodes)
-                       %>% as_tibble() %>%
-                         select(name,name_), by = "name") %>%
-            mutate(from = name_) %>% select(-name_) %>%
-            mutate(name = to) %>%
-            inner_join(graph1 %>% activate(nodes)
-                       %>% as_tibble() %>%
-                         select(name,name_), by = "name") %>%
-            mutate(to = name_) %>% select(-c(name_,name)),
-          directed = F
-          ) -> graph1
-
-graph1 %>% plot()
-
-
-### Graph2
-map_dfc(
-  seq(1,10,1),
-  function(.x) {
-    (dpois(seq(1,10,1),.x))}
+    (dnorm(seq(1,10,1),.x,.x))}
 ) |> as.matrix() |>
-  forceSymmetric() |>
+  forceSymmetric(uplo = "U") |>
   as.matrix() -> B
 
 B/sum(B) -> B
-
 colnames(B) <- (seq(.05,.95,.1))
 rownames(B) <- (seq(.05,.95,.1))
 
+(B^.5 / sum(B^.5)) -> G
+colnames(G) <- (seq(.05,.95,.1))
+rownames(G) <- (seq(.05,.95,.1))
+
+superheat::superheat(G,
+                     X.text = round(G, 3),
+                     X.text.size = 5,
+                     legend = FALSE,
+                     X.text.col = "white")
 superheat::superheat(B,
                      X.text = round(B, 3),
                      X.text.size = 5,
                      legend = FALSE,
                      X.text.col = "black")
-
-(B^.2 / sum(B^.2)) %>%
-  superheat::superheat(.,
-                       X.text = round(., 3),
-                       X.text.size = 5,
-                       legend = FALSE,
-                       X.text.col = "black")
-
-(B^.2 / sum(B^.2)) -> G
-
-#
-library(fastRG)
-library(tidygraph)
-
-latent <- fastRG::sbm(
-  n = V(graph1) %>% length(),
-  pi = dbetabinom(0:9,9,.225,.3),
-  B = G,
-  expected_degree = 20,
-  sort_nodes = T
-)
-
-dbetabinom(0:9,9,.225,.3)
-
-graph2 <- sample_tidygraph(latent) %>%
-  activate(edges) %>%
-  mutate(
-    color = "blue"
-  )
-
-graph2 %>%
-  activate(nodes) %>%
-  mutate(
-    beta = latent$z
-  ) %>% as_tibble() %>% mutate(
-    beta = abs((str_remove(beta,"block") %>%
-      as.integer()) - 10)/10 + .05
-  ) -> nodes
-
-nodes %>% group_by(beta) %>% count()
-nodes %>% count()
-
-### Union
-
-graph1 %>% as_tbl_graph() %>%
-  activate(nodes) %>% as_tibble() %>% View()
-
-graph1 %>% as_tbl_graph() %>%
-  activate(nodes) %>%
-  group_by(nucleus) %>%
-  mutate(alpha = rbetabinom(1,9,.225,.3),
-         alpha = (alpha/10) + .05) %>% ungroup() %>%
-  mutate(
-    beta = latent$z,
-    beta = abs((str_remove(beta,"block") %>%
-                         as.integer()) - 10)/10 + .05,
-    name = as.character(name) %>%
-      bind_edges(graph2%>%activate(edges)%>%as_tibble)
-    
-  ) %>%
-  inner_join(graph2 %>% activate(nodes) %>%
-               as_tibble(), by = "name") %>%
-  bind_edges(graph2%>%activate(edges)%>%as_tibble) %>%
-  activate(nodes) %>%
-  select(-nucleus_size) %>%
-  mutate(
-    nucleus = sample(nucleus, replace = F),
-    k = centrality_degree(),
-    neighs = local_members()) %>%
-  group_by(nucleus) %>%
-  mutate(nuclear_prop = (rbetabinom(1,9,.2,.2)+.5)/10) %>%
-  group_by(block) %>%
-  mutate(block_prop = str_remove(block,"block") %>%
-           as.integer(),
-         block_prop = abs(block_prop - 10.5)/10) %>%
-  ungroup() %>% group_by(name) %>%
-  mutate(
-    p = mean(c(nuclear_prop,block_prop)),
-    smoker = rbinom(1,1,p)) -> pop
+ggsave("B.png")
