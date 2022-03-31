3b4d5e01100a64e9db667ad92cc66323a259d411
giuliogcantone <prgcan@gmail.com>
2022-03-30 22:38:40 -0400

    ok

--- Results.R
+++ Results.R
@@ -32,19 +32,48 @@ results =
     err_hp = y_hp - y,
     err_yu = y_yu - y,
     err_hyu = y_hyu - y,
+    err_0 = y_0 - y,
     abs_gold = abs(err_gold),
     abs_p = abs(err_p),
     abs_hp = abs(err_hp),
     abs_yu = abs(err_yu),
     abs_hyu = abs(err_hyu),
-    abs_0 = abs(y_0 - y)
+    abs_0 = abs(y_0 - y),
+    delta_p = (abs_gold - abs_p)/y,
+    delta_yu = (abs_gold - abs_yu)/y,
+    delta_hp = (abs_0 - abs_hp)/y,
+    delta_hyu = (abs_0 - abs_hyu)/y
   ) %>% mutate(
     gamma_l = cut_number(gamma,4,
                          c("Low",
                            "Mid-Low",
                            "Mid-High",
-                           "High"))
+                           "High")),
+    ub_p = err_p + .01,
+    ub_yu = err_yu + .01,
+    ub_hp = err_hp + .01,
+    ub_hyu = err_hyu + .01,
+    delta_ubp = (abs_gold - abs(ub_p)) / y,
+    delta_ubyu = (abs_gold - abs(ub_yu)) / y,
+    delta_ubhp = (abs_gold - abs(ub_hp)) / y,
+    delta_ubhyu = (abs_gold - abs(ub_hyu)) / y
   )
+
+library(ggdensity)
+library(cowplot)
+ggdraw() +
+    draw_plot(
+      results %>% ggplot(aes(x=phi_y,
+                             y=phi_k)) +
+        geom_hdr() +
+        ylab(bquote(varphi[k])) +
+        xlab(bquote(varphi[y])) +
+        theme_linedraw(base_size = 30)+
+        theme(legend.position = "top")
+    ) +
+  draw_image(magick::image_read("gamma.png"),
+             scale = .25, x = .4, y = -.1)
+ggsave("phis.png")
 
 results %>%
   select(k,w,gamma,phi_y,phi_k,r,N,y,Q,abs_gold) %>%
@@ -88,14 +117,74 @@ results %>%
 results %>%
   select(k,w,gamma,phi_y,phi_k,Q,r,N,y,abs_gold) %>%
   names() %>%
-  paste('scale(abs_p)~',.)%>%
+  paste('scale(abs_hyu)~',.)%>%
   map_df(~tidy(lm(as.formula(.x), 
-                  data= results %>% select(-gamma_l) %>% mutate_all(scale)
+                  data= results %>% select(-gamma_l) %>%
+                    mutate_all(scale)
   ))) %>%
   filter(!term %>% str_detect("Intercept")) %>%
   arrange(p.value) %>% select(-statistic) %>%
   mutate_if(is.numeric,round,3)
 
+
+###
+
+results %>%
+  select(w,gamma,r,N,y,abs_gold) %>%
+  names() %>%
+  paste('scale(abs_p)~',.)%>%
+  map_df(~tidy(lm(as.formula(.x), 
+                  data= results %>% select(-gamma_l) %>% mutate_all(scale)
+  ))) %>%
+  filter(!term %>% str_detect("Intercept")) %>%
+  arrange(p.value) %>% select(term, estimate) %>%
+  mutate_if(is.numeric,round,3) %>%
+  left_join(
+    results %>%
+      select(w,gamma,r,N,y,abs_gold) %>%
+      names() %>%
+      paste('scale(abs_yu)~',.)%>%
+      map_df(~tidy(lm(as.formula(.x), 
+                      data= results %>% select(-gamma_l) %>% mutate_all(scale)
+      ))) %>%
+      filter(!term %>% str_detect("Intercept")) %>%
+      arrange(p.value) %>% select(term, estimate) %>%
+      mutate_if(is.numeric,round,3), by = "term"
+  ) %>%
+  left_join(
+    results %>%
+      select(w,gamma,r,N,y,abs_gold) %>%
+      names() %>%
+      paste('scale(abs_hp)~',.)%>%
+      map_df(~tidy(lm(as.formula(.x), 
+                      data= results %>% select(-gamma_l) %>% mutate_all(scale)
+      ))) %>%
+      filter(!term %>% str_detect("Intercept")) %>%
+      arrange(p.value) %>% select(term, estimate) %>%
+      mutate_if(is.numeric,round,3), by = "term"
+  ) %>%
+  left_join(
+    results %>%
+      select(w,gamma,r,N,y,abs_gold) %>%
+      names() %>%
+      paste('scale(abs_hyu)~',.)%>%
+      map_df(~tidy(lm(as.formula(.x), 
+                      data= results %>% select(-gamma_l) %>% mutate_all(scale)
+      ))) %>%
+      filter(!term %>% str_detect("Intercept")) %>%
+      arrange(p.value) %>% select(term, estimate) %>%
+      mutate_if(is.numeric,round,3), by = "term"
+  ) %>% add_column(.after = "term", Concept = c(
+    "Stage 0", "Homophily", "Familism",
+    "Attrition", "Target Quota", "Pop. Size"
+  )) %>%
+  rename(I = estimate.x,
+         II = estimate.y,
+         III = estimate.x.x,
+         IV = estimate.y.y) %>%
+  kableExtra::kable("latex")
+
+###
 
 results %>% summarise(
   cor = cor(gamma,phi_k),
@@ -113,26 +202,6 @@ results %>% ggplot() +
   theme_classic(base_size = 20)
 
 ###
-
-results %>% group_by(gamma_l) %>%
-  summarise(
-    phi_y = mean(phi_y),
-    phi_k = mean(phi_k),
-    Q = mean(Q))
-
-
-
-results %>% group_by(w_level) %>%
-  summarise(
-    n =n(),
-    ERR_gold = median(abs(ERR_gold)),
-    ERR_0 = median(abs(ERR_0)),
-    ERR_hp = median(abs(ERR_hp)),
-    ERR_hy = median(abs(ERR_hy)),
-    n_hp = median(n.hp),
-    n_hy = median(n.hy)
-  )
-
 
 results %>%
   mutate(id = cur_group_rows(), .before = "n.nuclei") %>%
